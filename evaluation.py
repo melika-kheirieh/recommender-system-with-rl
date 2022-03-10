b98d27895d970b1338d938bb41705ff602cfa0fc
melika-kheirieh <51747861+melika-kheirieh@users.noreply.github.com>
2022-03-10 11:44:52 +0330

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,3 @@
-
 from collections import defaultdict
 import os
 import pickle
@@ -19,6 +18,7 @@ from torch.utils.tensorboard import SummaryWriter
 
 from utils import (EvalDataset, OUNoise, Prioritized_Buffer, get_beta,
                    preprocess_data, to_np, hit_metric, dcg_metric)
+
 
 def run_evaluation(net, state_representation, training_env_memory, loader=valid_loader):
     hits = []
@@ -50,10 +50,81 @@ def run_evaluation(net, state_representation, training_env_memory, loader=valid_
 
 if __name__ == '__main__':
     valid_dataset = EvalDataset(
-    np.array(test_data)[np.array(test_data)[:, 0] == 49107],
-    item_num,
-    test_matrix)
+        np.array(test_data)[np.array(test_data)[:, 0] == 49107],
+        item_num,
+        test_matrix)
     valid_loader = td.DataLoader(valid_dataset, batch_size=100, shuffle=False)
 
     full_dataset = EvalDataset(np.array(test_data), item_num, test_matrix)
     full_loader = td.DataLoader(full_dataset, batch_size=100, shuffle=False)
+
+    with open('/content/drive/MyDrive/MIND-Dataset/MINDsmall_train/logs/memory.pickle', 'rb') as f:
+        memory = pickle.load(f)
+
+    no_ou_state_repr = State_Repr_Module(user_num, item_num, params['embedding_dim'], params['hidden_dim'])
+    no_ou_policy_net = Actor_DRR(params['embedding_dim'], params['hidden_dim'])
+    no_ou_state_repr.load_state_dict(torch.load(save_path + 'best_state_repr.pth'))
+    no_ou_policy_net.load_state_dict(torch.load(save_path + 'best_policy_net.pth'))
+
+    #no_ou_state_repr.load_state_dict(torch.load('logs/no_ou/' + 'best_state_repr.pth'))
+    #no_ou_policy_net.load_state_dict(torch.load('logs/no_ou/' + 'best_policy_net.pth'))
+
+    hit, dcg = run_evaluation(no_ou_policy_net, no_ou_state_repr, memory, full_loader)
+    print('hit rate: ', hit, 'dcg: ', dcg)
+
+
+    no_ou_state_repr = State_Repr_Module(user_num, item_num, params['embedding_dim'], params['hidden_dim'])
+    no_ou_policy_net = Actor_DRR(params['embedding_dim'], params['hidden_dim'])
+    no_ou_state_repr.load_state_dict(torch.load(save_path + 'state_repr_final.pth'))
+    no_ou_policy_net.load_state_dict(torch.load(save_path + 'policy_net_final.pth'))
+
+    #no_ou_state_repr.load_state_dict(torch.load('logs/no_ou/' + 'best_state_repr.pth'))
+    #no_ou_policy_net.load_state_dict(torch.load('logs/no_ou/' + 'best_policy_net.pth'))
+
+    hit, dcg = run_evaluation(no_ou_policy_net, no_ou_state_repr, memory, full_loader)
+    print('hit rate: ', hit, 'dcg: ', dcg)
+
+
+    ou_state_repr = State_Repr_Module(user_num, item_num, params['embedding_dim'], params['hidden_dim'])
+    ou_policy_net = Actor_DRR(params['embedding_dim'], params['hidden_dim'])
+    ou_state_repr.load_state_dict(torch.load(save_path + 'ou_noise_04/' + 'state_repr_final.pth'))
+    ou_policy_net.load_state_dict(torch.load(save_path + 'ou_noise_04/' + 'policy_net_final.pth'))
+
+    hit, dcg = run_evaluation(ou_policy_net, ou_state_repr, memory, full_loader)
+    print('hit rate: ', hit, 'dcg: ', dcg)
+
+
+    movies = pd.read_csv('data/movies.dat', sep='::', header=None, engine='python', names=['id', 'name', 'genre'])
+    # in the code numeration starts with 0
+    movies[movies['id'].isin(np.argwhere(test_matrix[random_user] > 0)[:, 1] + 1)]
+
+    np.argwhere(test_matrix[random_user] > 0)[:, 1]
+
+    predictions = []
+
+    for model, state_representation in zip([ou_policy_net, no_ou_policy_net], [ou_state_repr, no_ou_state_repr]):
+        example_env = Env(test_matrix)
+        user, memory = example_env.reset(random_user)
+
+        user, memory, reward, _ = example_env.step(torch.tensor([13]))
+        user, memory, reward, _ = example_env.step(torch.tensor([1584]))
+        preds = []
+        for _ in range(3):
+            action_emb = model(state_representation(user, memory))
+            action = model.get_action(
+                user,
+                torch.tensor(example_env.memory[to_np(user).astype(int), :]),
+                state_representation,
+                action_emb,
+                torch.tensor(
+                    [item for item in example_env.available_items
+                    if item not in example_env.viewed_items]
+                ).long()
+            )
+            user, memory, reward, _ = example_env.step(action)
+            preds.append(action)
+
+        predictions.append(preds)
+
+    print(predictions[0])
+    print(predictions[1])
